344e2cd93604f26220c943eb84ff96674960d817
Richard Wang <zhwang@live.com.au>
2014-05-27 13:48:15 +1000

    Add multiplier config
    Closes #2

--- index.js
+++ index.js
@@ -2,25 +2,34 @@ var EditorView = require('atom').EditorView,
     origHandleEvents = EditorView.prototype.handleEvents;
 
 module.exports = {
+  configDefaults: {
+    multiplier: '0.3'
+  },
+
   activate: function(state) {
     EditorView.prototype.handleEvents = function() {
+      var multiplier = parseFloat(atom.config.get('woah-there-mousewheel.multiplier')) || 0.3;
+
       if (!this.mini) {
         this.scrollView.on('mousewheel', (function(_this) {
           return function(e) {
             var delta = e.originalEvent.wheelDeltaY;
             if (delta != 0) {
-              // Cancel out 0.7 of the original scroll change to make it delta * 0.3
-              _this.scrollTop(_this.scrollTop() + delta * 0.7);
+              // The original scroll multiplier is -1 so apply +1-multiplier
+              _this.scrollTop(_this.scrollTop() + delta * (1-multiplier));
               return false;
             }
           };
         })(this));
       }
+
       origHandleEvents.call(this);
     };
   },
+
   deactivate: function() {
     EditorView.prototype.handleEvents = origHandleEvents;
   }
+
   // TODO: bind/unbind currently open editorviews
 };
